d9ac990d9eb833f6d6c613b0c4fd4a4cb7955c59
Alejandro Erickson <alejandro.erickson@gmail.com>
2016-06-15 20:36:28 +0100

    evil-initialize emms

--- layers/media/packages.el
+++ layers/media/packages.el
@@ -75,6 +75,10 @@ Each entry is either:
   (use-package emms
     :init
     (global-set-key [(f7)] 'emms-smart-browse)
+    (add-hook 'emms-browser-show-display-hook 'evil-emms-keymap-mode)
+    (add-hook 'emms-browser-show-display-hook 'evil-initialize)
+    (evil-set-initial-state 'emms-playlist-mode 'emacs)
+    (evil-set-initial-state 'emms-browser-mode 'normal)
     :config
     ;;(require 'emms-setup)
     (emms-all)
@@ -89,81 +93,78 @@ Each entry is either:
     ;; PROBLEMS BEGIN HERE
 
     ;; this works fine
-    (evil-set-initial-state 'emms-playlist-mode 'emacs)
 
     ;;this doesn't work.
-    (evil-set-initial-state 'emms-browser-mode 'emacs)
     ;; It might be because emms-browser-mode is not a real mode and mode-hooks
     ;; don't work here
     
     ;; As a result, evilifying didn't work
-    ;; (evilified-state-evilify-map emms-browser-mode-map
-    ;;   :mode emms-browser-mode
-    ;;   :bindings
-    ;;  "r" 'emms-browser-goto-random 
-    ;;  )
+    (evilified-state-evilify-map emms-browser-mode-map
+      :mode emms-browser-mode
+   ;;   :bindings
+     ;;"r" 'emms-browser-goto-random 
+     )
 
-    ;; and this doesn't work
-    (define-minor-mode evil-emms-keymap-mode
-      "Evil EMMS Key map mode."
-      :keymap (make-sparse-keymap)
-      )
-   (evil-define-key 'normal evil-emms-keymap-mode-map "b1" 'emms-browse-by-artist)
-   (evil-define-key 'normal evil-emms-keymap-mode-map "b2" 'emms-browse-by-artist)
-;; (evil-define-key 'normal evil-emms-keymap-mode "C-j" 'emms-browser-add-tracks-and-play)
-;; (evil-define-key 'normal evil-emms-keymap-mode "RET" 'emms-browser-add-tracks)
-;; (evil-define-key 'normal evil-emms-keymap-mode "SPC" 'emms-browser-toggle-subitems)
-;; (evil-define-key 'normal evil-emms-keymap-mode "/" 'emms-isearch-buffer)
-(evil-define-key 'normal evil-emms-keymap-mode "1" 'emms-browser-collapse-all)
-(evil-define-key 'normal evil-emms-keymap-mode "2" 'emms-browser-expand-to-level-2)
-(evil-define-key 'normal evil-emms-keymap-mode "3" 'emms-browser-expand-to-level-3)
-(evil-define-key 'normal evil-emms-keymap-mode "4" 'emms-browser-expand-to-level-4)
-(evil-define-key 'normal evil-emms-keymap-mode "<" 'emms-browser-previous-filter)
-(evil-define-key 'normal evil-emms-keymap-mode ">" 'emms-browser-next-filter)
-(evil-define-key 'normal evil-emms-keymap-mode "?" 'describe-mode)
-(evil-define-key 'normal evil-emms-keymap-mode "C" 'emms-browser-clear-playlist)
-(evil-define-key 'normal evil-emms-keymap-mode "E" 'emms-browser-expand-all)
-(evil-define-key 'normal evil-emms-keymap-mode "d" 'emms-browser-view-in-dired)
-(evil-define-key 'normal evil-emms-keymap-mode "D" 'emms-browser-delete-files)
-(evil-define-key 'normal evil-emms-keymap-mode "q" 'emms-browser-bury-buffer)
-(evil-define-key 'normal evil-emms-keymap-mode "r" 'emms-browser-goto-random)
-(evil-define-key 'normal evil-emms-keymap-mode "n" 'next-line)
-(evil-define-key 'normal evil-emms-keymap-mode "p" 'previous-line)
-;; (evil-define-key 'normal evil-emms-keymap-mode "C-/" 'emms-playlist-mode-undo)
-;; (evil-define-key 'normal evil-emms-keymap-mode "<C-return>" 'emms-browser-add-tracks-and-play)
-;; (evil-define-key 'normal evil-emms-keymap-mode "<backtab>" 'emms-browser-prev-non-track)
-;; (evil-define-key 'normal evil-emms-keymap-mode "<tab>" 'emms-browser-next-non-track)
+;;     ;; and this doesn't work
+;;     (define-minor-mode evil-emms-keymap-mode
+;;       "Evil EMMS Key map mode."
+;;       :keymap (make-sparse-keymap)
+;;       )
+;;    (evil-define-key 'normal evil-emms-keymap-mode-map "b1" 'emms-browse-by-artist)
+;;    (evil-define-key 'normal evil-emms-keymap-mode-map "b2" 'emms-browse-by-artist)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "C-j" 'emms-browser-add-tracks-and-play)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "RET" 'emms-browser-add-tracks)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "SPC" 'emms-browser-toggle-subitems)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "/" 'emms-isearch-buffer)
+;; (evil-define-key 'normal evil-emms-keymap-mode "1" 'emms-browser-collapse-all)
+;; (evil-define-key 'normal evil-emms-keymap-mode "2" 'emms-browser-expand-to-level-2)
+;; (evil-define-key 'normal evil-emms-keymap-mode "3" 'emms-browser-expand-to-level-3)
+;; (evil-define-key 'normal evil-emms-keymap-mode "4" 'emms-browser-expand-to-level-4)
+;; (evil-define-key 'normal evil-emms-keymap-mode "<" 'emms-browser-previous-filter)
+;; (evil-define-key 'normal evil-emms-keymap-mode ">" 'emms-browser-next-filter)
+;; (evil-define-key 'normal evil-emms-keymap-mode "?" 'describe-mode)
+;; (evil-define-key 'normal evil-emms-keymap-mode "C" 'emms-browser-clear-playlist)
+;; (evil-define-key 'normal evil-emms-keymap-mode "E" 'emms-browser-expand-all)
+;; (evil-define-key 'normal evil-emms-keymap-mode "d" 'emms-browser-view-in-dired)
+;; (evil-define-key 'normal evil-emms-keymap-mode "D" 'emms-browser-delete-files)
+;; (evil-define-key 'normal evil-emms-keymap-mode "q" 'emms-browser-bury-buffer)
+;; (evil-define-key 'normal evil-emms-keymap-mode "r" 'emms-browser-goto-random)
+;; (evil-define-key 'normal evil-emms-keymap-mode "n" 'next-line)
+;; (evil-define-key 'normal evil-emms-keymap-mode "p" 'previous-line)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "C-/" 'emms-playlist-mode-undo)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "<C-return>" 'emms-browser-add-tracks-and-play)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "<backtab>" 'emms-browser-prev-non-track)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "<tab>" 'emms-browser-next-non-track)
 
-;; (evil-define-key 'normal evil-emms-keymap-mode "sA" 'emms-browser-search-by-album)
-;; (evil-define-key 'normal evil-emms-keymap-mode "sa" 'emms-browser-search-by-artist)
-;; (evil-define-key 'normal evil-emms-keymap-mode "sc" 'emms-browser-search-by-composer)
-;; (evil-define-key 'normal evil-emms-keymap-mode "ss" 'emms-browser-search-by-names)
-;; (evil-define-key 'normal evil-emms-keymap-mode "st" 'emms-browser-search-by-title)
-;; (evil-define-key 'normal evil-emms-keymap-mode "sp" 'emms-browser-search-by-performer)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "sA" 'emms-browser-search-by-album)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "sa" 'emms-browser-search-by-artist)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "sc" 'emms-browser-search-by-composer)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "ss" 'emms-browser-search-by-names)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "st" 'emms-browser-search-by-title)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "sp" 'emms-browser-search-by-performer)
 
-;; (evil-define-key 'normal evil-emms-keymap-mode "b1" 'emms-browse-by-artist)
-;; (evil-define-key 'normal evil-emms-keymap-mode "b2" 'emms-browse-by-album)
-;; (evil-define-key 'normal evil-emms-keymap-mode "b3" 'emms-browse-by-genre)
-;; (evil-define-key 'normal evil-emms-keymap-mode "b4" 'emms-browse-by-year)
-;; (evil-define-key 'normal evil-emms-keymap-mode "b5" 'emms-browse-by-composer)
-;; (evil-define-key 'normal evil-emms-keymap-mode "b6" 'emms-browse-by-performer)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "b1" 'emms-browse-by-artist)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "b2" 'emms-browse-by-album)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "b3" 'emms-browse-by-genre)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "b4" 'emms-browse-by-year)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "b5" 'emms-browse-by-composer)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "b6" 'emms-browse-by-performer)
 
-;; (evil-define-key 'normal evil-emms-keymap-mode "Wap" 'emms-browser-lookup-album-on-pitchfork)
-;; (evil-define-key 'normal evil-emms-keymap-mode "Waw" 'emms-browser-lookup-album-on-wikipedia)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "Wap" 'emms-browser-lookup-album-on-pitchfork)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "Waw" 'emms-browser-lookup-album-on-wikipedia)
 
-;; (evil-define-key 'normal evil-emms-keymap-mode "WAp" 'emms-browser-lookup-artist-on-pitchfork)
-;; (evil-define-key 'normal evil-emms-keymap-mode "WAw" 'emms-browser-lookup-artist-on-wikipedia)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "WAp" 'emms-browser-lookup-artist-on-pitchfork)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "WAw" 'emms-browser-lookup-artist-on-wikipedia)
 
-;; (evil-define-key 'normal evil-emms-keymap-mode "WCp" 'emms-browser-lookup-composer-on-pitchfork)
-;; (evil-define-key 'normal evil-emms-keymap-mode "WCw" 'emms-browser-lookup-composer-on-wikipedia)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "WCp" 'emms-browser-lookup-composer-on-pitchfork)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "WCw" 'emms-browser-lookup-composer-on-wikipedia)
 
-;; (evil-define-key 'normal evil-emms-keymap-mode "WPp" 'emms-browser-lookup-performer-on-pitchfork)
-;; (evil-define-key 'normal evil-emms-keymap-mode "WPw" 'emms-browser-lookup-performer-on-wikipedia)
-   (add-hook 'emms-browser-show-display-hook 'evil-emms-keymap-mode)
-    ;; "T" 'emms-browser-delete-files-by-moving-to-trash
-;;      "RET"
-;;      "SPC"
-     ;;  )
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "WPp" 'emms-browser-lookup-performer-on-pitchfork)
+;; ;; (evil-define-key 'normal evil-emms-keymap-mode "WPw" 'emms-browser-lookup-performer-on-wikipedia)
+;;     ;; "T" 'emms-browser-delete-files-by-moving-to-trash
+;; ;;      "RET"
+;; ;;      "SPC"
+;;      ;;  )
     )
   )
 ;;(defun media/init-emms-info-libtag ()
