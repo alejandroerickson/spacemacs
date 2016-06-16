b4d12a94314a89908069d2aaa3252a0a78ac0163
Alejandro Erickson <alejandro.erickson@gmail.com>
2016-06-16 11:05:05 +0100

    key bindings, license, logo size

--- layers/media/funcs.el
+++ layers/media/funcs.el
@@ -1,4 +1,15 @@
 
+;;; funcs.el --- media layer functions file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Alejandro Erickson <alejandro.erickson@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
     (defun emms-browser-delete-files-by-moving-to-trash ()
       "Move all files under point to trash.
 Disabled by default."
