18c019b6087220614bc75e888ab1762433c99fa3
Alejandro Erickson <alejandro.erickson@gmail.com>
2016-06-23 11:57:43 +0100

    Enable sage-shell-mode extensions, disable sage-mode
    Sage-mode from bitbucket/gvol disabled in favour of sage-shell-mode.
    Disable auto-complete-sage enable helm-sage.  Beginnings of a leader-key
    map.

--- layers/+lang/sagemath/packages.el
+++ layers/+lang/sagemath/packages.el
@@ -32,9 +32,17 @@
 (defconst sagemath-packages
   '(
     sage-shell-mode
-    (sage-mode :location (recipe
-                          :fetcher bitbucket
-                          :repo "gvol/sage-mode"))
+    auto-complete-sage
+    helm-sage
+    anything-sage
+    ob-sagemath
+
+    ;; sage-shell-mode contains a copy of sage(-mode).
+    ;; If it breaks, then enabling this is your backup plan.
+    ;; (sage :location (recipe
+    ;;                       :fetcher bitbucket
+    ;;                       :repo "gvol/sage-mode"
+    ;;                       :files ("emacs/*")))
     )
   "The list of Lisp packages required by the sagemath layer.
 
@@ -65,12 +73,61 @@ Each entry is either:
 
 
 (defun sagemath/init-sage-shell-mode ()
-  (use-package sage-shell-mode)
-  :defer t
+  (use-package sage-shell-mode
+    :defer t
+    :config
+    (progn
+      ;;TODO M-n and M-p to jk. The major-mode of the Sage process buffer is
+      ;;sage-shell-mode
+      (spacemacs/set-leader-keys-for-major-mode 'sage-shell-mode
+        "c" 'sage-shell:interrupt-subjob
+        "o" 'sage-shell:delete-output
+        "O" 'sage-shell:clear-current-buffer
+        "l" 'sage-shell:load-file
+        "?" 'sage-shell:help
+        "b" 'sage-shell:list-outputs
+        "w" 'sage-shell:copy-previous-output-to-kill-ring
+        "hh" 'helm-sage-complete
+        )
+      (evil-define-key 'insert 'sage-shell-mode-map (kbd "tab") 'helm-sage-complete)
+      ;; Enable setting of executable
+      (setq sage-shell:completion-function 'helm-sage)
+      (spacemacs/set-leader-keys-for-major-mode 'sage-shell:sage-mode
+        "s" 'sage-shell:run-sage
+        "S" 'sage-shell:run-new-sage
+        )
+      ;; is this working?
+      (setq sage-shell:input-history-cache-file "~/.emacs.d/.cache/.sage_shell_input_history")
+      )
+    )
   )
-(defun sagemath/init-sage-mode ()
-  (use-package sage-mode
+
+;; sage-shell-mode contains a copy of sage(-mode).
+;; If it breaks, then enabling this is your backup plan.
+;;(defun sagemath/init-sage ()
+  ;; (use-package sage
+  ;;   :defer t
+;; :config
+;; (add-hook 'sage-shell:sage-mode-hook 'ac-sage-setup)
+;; (add-hook 'sage-shell-mode-hook 'ac-sage-setup)
+  ;;   )
+  ;; )
+
+;; sage-shell-mode can be extended with auto-complete as well, if you don't like helm
+;; (defun sagemath/init-auto-complete-sage ()
+;;   (use-package auto-complete-sage)
+;;   :defer t
+;;   :config
+;;   (progn
+;;     ;;(setq sage-shell:completion-function 'completion-at-point)
+;;     )
+;;   )
+
+(defun sagemath/init-helm-sage ()
+  (use-package helm-sage
     :defer t
     )
   )
+;;ob-sagemath
+
 ;;; packages.el ends here
